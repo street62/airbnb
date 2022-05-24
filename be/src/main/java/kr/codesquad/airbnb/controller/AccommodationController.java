286946d4d94c75c651c721e19356bb70d0f21f87
bugpigg <bugpigg@gmail.com>
2022-05-24 12:16:16 +0900

    [#4] Feat: 숙소 예약 컨트롤러 작성
    - AccommodationController의 postReservation() 메서드로 숙소 예약 요청.

--- be/src/main/java/kr/codesquad/airbnb/controller/AccommodationController.java
+++ be/src/main/java/kr/codesquad/airbnb/controller/AccommodationController.java
@@ -1,10 +1,14 @@
 package kr.codesquad.airbnb.controller;
 
 import kr.codesquad.airbnb.dto.ReserveFormResponseDto;
+import kr.codesquad.airbnb.dto.ReserveRequestDto;
 import kr.codesquad.airbnb.dto.SearchQueryResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,10 @@ public class AccommodationController {
     public ResponseEntity<?> getReserveForm(@PathVariable String accommodationId) {
         return ResponseEntity.ok(new ReserveFormResponseDto());
     }
+
+    @PostMapping("/reserve/{accommodationId}")
+    public HttpStatus postReservation(@RequestBody ReserveRequestDto requestDto) {
+        return HttpStatus.OK;
+    }
+
 }
