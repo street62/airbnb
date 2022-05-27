92351921b203217dd83c5b805b881ad8bb2cea59
Seungun Jang <bugpigg@gmail.com>
2022-05-27 12:28:25 +0900

    숙소 검색 서비스 로직을 DB 쿼리문으로 변경 (#38)
    * Feat: AccommodationRepository 검색 메소드 추가
    
    - AccommodationRepository 에 입력된 검색 조건에 따라 검색을 수행하는 메소드를 추가하였습니다.
    
    * Feat: AccommodationService search() 메소드 로직 수정
    
    - 기존의 자바 코드 기반으로 검색을 수행하던 로직을, DB의 쿼리 기반으로 로직을 수행하게 변경하였습니다.
    
    * Refactor: Accommodation 코드 포맷팅 수정
    
    - 실수로 대괄호가 하나 들어가 있어 삭제하였습니다.

--- be/src/main/java/kr/codesquad/airbnb/domain/Accommodation.java
+++ be/src/main/java/kr/codesquad/airbnb/domain/Accommodation.java
@@ -1,22 +1,26 @@
 package kr.codesquad.airbnb.domain;
 
-import java.time.LocalDate;
 import java.util.List;
-
-import lombok.Builder;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Accommodation {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @JoinColumn(name="DISCOUNT_POLICY_ID")
+    @JoinColumn(name = "DISCOUNT_POLICY_ID")
     private DiscountPolicy discountPolicy;
     private String name;
     private String imgUrl;
@@ -28,5 +32,4 @@ public class Accommodation {
     private double coordinateX;
     private double coordinateY;
     private int cleaningFee;
-    }
 }
--- be/src/main/java/kr/codesquad/airbnb/repository/AccommodationRepository.java
+++ be/src/main/java/kr/codesquad/airbnb/repository/AccommodationRepository.java
@@ -1,13 +1,49 @@
 package kr.codesquad.airbnb.repository;
 
+import java.time.LocalDate;
+import kr.codesquad.airbnb.domain.Accommodation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AccommodationRepository extends JpaRepository {
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+
+    @Query(value = ""
+        + "SELECT AC2"
+        + "FROM"
+
+        + "(SELECT AC1"
+        + "FROM Accommodation AS AC1"
+        + "WHERE "
+        + ":minPrice <= AC.feePerOneNight AND"
+        + "AC.feePerOneNight <= :maxPrice AND"
+        + ":adultChildCapacity <= AC.adultChildCapacity AND"
+        + ":infantCount <= AC.infantCapacity"
+        + ") AS AC2 "
+
+        + "LEFT JOIN"
+
+        + "(SELECT "
+        + "FROM Reservation AS RS1"
+        + "WHERE"
+        + "RS1.checkinDate <= :checkinDate AND"
+        + ":checkoutDate <= RS1.checkoutDate"
+        + ") AS RS2"
+
+        + "ON AC2.id = RS2.accommodation_id"
+        )
+    List<Accommodation> findAllBySearchCondition(
+        @Param("checkinDate") LocalDate checkinDate,
+        @Param("checkoutDate") LocalDate checkoutDate,
+        @Param("minPrice") int minPrice,
+        @Param("maxPrice") int maxPrice,
+        @Param("adultChildCount") int adultCount,
+        @Param("infantCount") int infantCount
+        );
 
     @Query(value = "SELECT fee_per_one_night FROM Accommodation", nativeQuery = true)
     List<Integer> findAllprices();
