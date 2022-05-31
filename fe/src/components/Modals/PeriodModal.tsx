9784e99c0e33f90848b0a1b52a43856a19cbb0b6
SeungHyun <fm10033@gmail.com>
2022-05-31 14:06:15 +0900

    Design : 달력 기본 레이아웃 구현

--- fe/src/components/Modals/PeriodModal.tsx
+++ fe/src/components/Modals/PeriodModal.tsx
@@ -1,27 +1,25 @@
 import { usePeriodDispatch } from 'contexts/periodContext';
 import styled from 'styled-components';
+import { ReactComponent as LeftIcon } from 'images/FE_숙소예약서비스/Property 1=chevron-left.svg';
+import { ReactComponent as RightIcon } from 'images/FE_숙소예약서비스/Property 1=chevron-right.svg';
+
 import { ModalWrap } from './styled';
 
 function PeriodModal() {
   const { setCheckIn, setCheckOut } = usePeriodDispatch();
-  const date = new Date();
 
   return (
     <PeriodModalWrap>
-      <CheckInBtn
-        onClick={() => {
-          setCheckIn('체크인');
-        }}
-      >
-        체크인
-      </CheckInBtn>
-      <CheckOutBtn
-        onClick={() => {
-          setCheckOut('체크아웃');
-        }}
-      >
-        체크아웃
-      </CheckOutBtn>
+      <SlideBtnWrap>
+        <PrevBtn />
+        <ThisMonthTitle>이번달</ThisMonthTitle>
+        <NextMonthTitle>다음달</NextMonthTitle>
+        <NextBtn />
+      </SlideBtnWrap>
+      <Calendar>
+        <ThisMonth>이번달</ThisMonth>
+        <NextMonth>다음달</NextMonth>
+      </Calendar>
     </PeriodModalWrap>
   );
 }
@@ -29,8 +27,46 @@ function PeriodModal() {
 const PeriodModalWrap = styled(ModalWrap)`
   width: 916px;
   height: 512px;
-  display: none;
+  padding: 64px 88px;
+  flex-direction: column;
+  /* display: none; */
 `;
-const CheckInBtn = styled.button``;
-const CheckOutBtn = styled(CheckInBtn)``;
+const SlideBtnWrap = styled.div`
+  width: 100%;
+  height: 24px;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  margin-bottom: 24px;
+`;
+const PrevBtn = styled(LeftIcon)`
+  position: absolute;
+`;
+const ThisMonthTitle = styled.div`
+  width: 336px;
+  display: flex;
+  justify-content: center;
+  ${({ theme }) => theme.fontStyles.bold16px};
+`;
+const NextMonthTitle = styled(ThisMonthTitle)``;
+const NextBtn = styled(RightIcon)`
+  position: absolute;
+  right: 0;
+`;
+const Calendar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const ThisMonth = styled.div`
+  width: 336px;
+  height: 336px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const NextMonth = styled(ThisMonth)``;
+// const CheckInBtn = styled.button``;
+// const CheckOutBtn = styled(CheckInBtn)``;
+
 export default PeriodModal;
