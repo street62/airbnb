f19bd2ab48769ade6e9db595eaa42ad134c4378c
Seungun Jang <bugpigg@gmail.com>
2022-05-31 16:22:23 +0900

    AccommodationController의 RegisterReservation() 반환타입 변경 및 ReserveRequestDto의 날짜 매핑 어노테이션 추가 (#60)
    * Fix: ReserveRequestDto의 날짜 매핑 어노테이션 추가
    
    - 기존 날짜 매핑 어노테이션이 빠져 있어, ReserveRequestDto의 날짜 매핑 어노테이션을 추가하였습니다.
    
    * Feat: AccommodationController의 RegisterReservation() 반환타입 변경
    
    - 기존 HttpStatus를 반환하던 RegisterReservation() 반환타입을 ResponseEntity 로 수정하였습니다.

--- be/src/main/java/kr/codesquad/airbnb/dto/ReserveRequestDto.java
+++ be/src/main/java/kr/codesquad/airbnb/dto/ReserveRequestDto.java
@@ -4,15 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReserveRequestDto {
 
     private Long userId;
     private int feePerOneNight;
     private int nightNumber;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate checkinDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate checkoutDate;
     private int cleaningFee;
     private int serviceCommission;
