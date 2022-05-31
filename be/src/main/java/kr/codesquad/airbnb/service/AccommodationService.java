28b3689ef69f9de3e1f003cb564686433aed417f
bugpigg <bugpigg@gmail.com>
2022-05-31 14:47:07 +0900

    Feat: Member, Accommodation 엔티티 검색 실패 예외 추가 및 서비스 클래스에 반영
    - MemberNotFoundException, AccommodationNotFoundException 클래스를 추가하였습니다.
    - 추가한 예외들을 AccommodationService 클래스에 반영하였습니다.

--- be/src/main/java/kr/codesquad/airbnb/service/AccommodationService.java
+++ be/src/main/java/kr/codesquad/airbnb/service/AccommodationService.java
@@ -3,9 +3,11 @@ package kr.codesquad.airbnb.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import kr.codesquad.airbnb.domain.Accommodation;
-import kr.codesquad.airbnb.domain.Member;
+import kr.codesquad.airbnb.domain.accommodation.Accommodation;
+import kr.codesquad.airbnb.domain.member.Member;
 import kr.codesquad.airbnb.domain.Reservation;
+import kr.codesquad.airbnb.domain.accommodation.AccommodationNotFoundException;
+import kr.codesquad.airbnb.domain.member.MemberNotFoundException;
 import kr.codesquad.airbnb.dto.ReserveFormResponseDto;
 import kr.codesquad.airbnb.dto.ReserveRequestDto;
 import kr.codesquad.airbnb.dto.SearchQueryRequestDto;
@@ -45,13 +47,13 @@ public class AccommodationService {
     public ReserveFormResponseDto getReservationPage(String accommodationId) {
         Accommodation accommodation = accommodationRepository.findById(
                         Long.valueOf(accommodationId))
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(AccommodationNotFoundException::new);
         return ReserveFormResponseDto.of(accommodation);
     }
 
     public HttpStatus generateNewReservation(ReserveRequestDto requestDto, Long accommodationId) {
         Member member = memberRepository.findById(requestDto.getUserId())
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(MemberNotFoundException::new);
         Accommodation accommodation = accommodationRepository.findById(accommodationId)
                 .orElseThrow(RuntimeException::new);
         Reservation reservation = Reservation.builder()
