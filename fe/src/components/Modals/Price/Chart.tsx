4e51fe6916505c7699b58b38ed91d327b2e246c1
jm <jm.dev.study@gmail.com>
2022-06-03 02:46:51 +0900

    Feat: 곡선 차트 구현

--- fe/src/components/Modals/Price/Chart.tsx
+++ fe/src/components/Modals/Price/Chart.tsx
@@ -6,9 +6,14 @@ type PriceInfoType = {
   priceData: Array<number>;
 };
 
+type ObjType = {
+  [key: string]: number;
+};
+
 const CANVAS_INFO = {
   WIDTH: 365,
   HEIGHT: 100,
+  SECTIONS: 15,
 };
 
 function Chart({ minPrice, maxPrice, priceData }: PriceInfoType) {
@@ -21,13 +26,71 @@ function Chart({ minPrice, maxPrice, priceData }: PriceInfoType) {
     const ctx = canvas.getContext('2d');
     if (!ctx) return;
 
-    const { WIDTH, HEIGHT } = CANVAS_INFO;
+    const { WIDTH, HEIGHT, SECTIONS } = CANVAS_INFO;
+    const getRange = (data: Array<number>) => {
+      const 고정범위 = Math.floor((maxPrice - minPrice) / SECTIONS);
+      let 현재범위 = 고정범위;
 
-    // 미완성
+      const obj: ObjType = {};
+      for (let range = 고정범위; range < maxPrice - minPrice; range += 고정범위) {
+        obj[range] = 0;
+      }
+
+      data.forEach((el) => {
+        if (현재범위 < el) {
+          현재범위 += 고정범위;
+
+          if (현재범위 + 고정범위 < el) {
+            // eslint-disable-next-line no-unreachable-loop
+            do {
+              현재범위 += 고정범위;
+              obj[현재범위] = 1;
+              return;
+            } while (현재범위 > el && maxPrice > 현재범위);
+          }
+        }
+
+        !obj[현재범위] ? (obj[현재범위] = 1) : (obj[현재범위] += 1);
+      });
+
+      return obj;
+    };
+
+    const dataRange = getRange(priceData);
+
+    const drawLine = (dataRange: ObjType) => {
+      const dataValues = Object.values(dataRange);
+
+      dataValues.forEach((data: number, idx: number) => {
+        const sectionWidth = Math.floor(WIDTH / (SECTIONS - 1));
+        const perY = Math.floor((data / Math.max(...dataValues)) * HEIGHT);
+
+        const moveCoord = { x: sectionWidth * idx, y: HEIGHT - perY };
+        const controlPoint = {
+          x: moveCoord.x - sectionWidth / 2,
+          y: idx === 0 ? HEIGHT : HEIGHT - (dataValues[idx - 1] / Math.max(...dataValues)) * HEIGHT,
+        };
+
+        ctx.bezierCurveTo(
+          controlPoint.x,
+          controlPoint.y,
+          controlPoint.x,
+          moveCoord.y,
+          moveCoord.x,
+          moveCoord.y,
+        );
+      });
+    };
+
     ctx.beginPath();
     ctx.moveTo(0, HEIGHT);
+    drawLine(dataRange);
     ctx.lineTo(WIDTH, HEIGHT);
-    ctx.stroke();
+
+    const gradient = ctx.createLinearGradient(0, HEIGHT, WIDTH, HEIGHT);
+    gradient.addColorStop(0, '#E0E0E0');
+    gradient.addColorStop(1, 'black');
+    ctx.fillStyle = gradient;
     ctx.fill();
   };
 
