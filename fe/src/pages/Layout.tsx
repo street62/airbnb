4f4314b994e79b6aceeecc1f10df40a6e077c408
jm <jm.dev.study@gmail.com>
2022-06-07 22:59:56 +0900

    Feat: result 페이지의 큰 서치바의 외부영역을 클릭하면 작은서치바로 돌아오는 기능 추가

--- fe/src/pages/Layout.tsx
+++ fe/src/pages/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 import Header from 'components/Header/index';
 import styled from 'styled-components';
 import { ModalProvider } from 'contexts/ModalContext';
+import { useRef } from 'react';
 
 function Layout() {
+  const containerRef = useRef<HTMLDivElement>(null);
   return (
     <ModalProvider>
-      <Container>
-        <Header />
+      <Container ref={containerRef}>
+        <Header containerRef={containerRef} />
         <Outlet />
       </Container>
     </ModalProvider>
