88b3f041ac047004b981b80b88aaf65234828c91
jm <jm.dev.study@gmail.com>
2022-06-08 22:50:55 +0900

    Fix: 게스트가 0명이면 인원 선택 텍스트가 보이도록 수정 + 함수명 수정

--- fe/src/contexts/PersonnelContext.tsx
+++ fe/src/contexts/PersonnelContext.tsx
@@ -92,8 +92,8 @@ export function usePersonnelDispatch() {
 
   const increaseCount = (age: string) => dispatch({ type: 'INCREASE_COUNT', age });
   const decreaseCount = (age: string) => dispatch({ type: 'DECREASE_COUNT', age });
-  const setText = (text: string) => dispatch({ type: 'SET_COUNTER_TEXT', text });
+  const setPersonnelText = (text: string) => dispatch({ type: 'SET_COUNTER_TEXT', text });
   const resetCount = () => dispatch({ type: 'RESET_COUNT', payload: null });
 
-  return { increaseCount, decreaseCount, setText, resetCount };
+  return { increaseCount, decreaseCount, setPersonnelText, resetCount };
 }
