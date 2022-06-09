05646b02d214833e3707b65baf54d34acb7ae502
도비 <85747667+JiminKim-dev@users.noreply.github.com>
2022-06-10 02:14:07 +0900

    [FE] 필터에 해당하는 숙소 렌더링, 지도 마커 띄우기, 지도 드래그 활성화, 지도 줌인아웃 구현 (#95)
    * Feat: 검색버튼 클릭시 필터에 해당하는 url로 이동하도록 구현
    
    * Chore: axios 설치
    
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>
    
    * Feat: 검색 버튼을 클릭하면 해당하는 정보의 숙소를 렌더링한다.
    
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>
    
    * Fix: 유틸함수 makeDateString 수정
    
    * Fix: 검색결과에 따른 필터 텍스트 설정
    
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>
    
    * Fix: url이 바뀌면 데이터가 다시 업데이트 되도록 수정
    
    * Feat: 지도에 숙소에 대한 가격 마커 표시
    
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>
    
    * Feat: 지도 드래그 활성화 기능 및 줌인아웃 구현
    
    Co-authored-by: herrakam <herrakam@users.noreply.github.com>

--- fe/src/components/HotelList/index.tsx
+++ fe/src/components/HotelList/index.tsx
@@ -1,48 +1,49 @@
 import React, { Fragment } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import styled from 'styled-components';
 import { Divider } from '@mui/material';
 
 import Hotel from 'components/HotelList/Hotel';
+import FilterText from 'components/HotelList/Filter';
+import Reservation from 'components/Modals/Reservation';
 import SkeletonHotel from 'components/Skeleton/Hotel';
 
 import { useModal } from 'hooks/useModal';
+import { useGetFetch } from 'hooks/useFetch';
 
-import { keyMaker } from 'utils/util';
-import Reservation from 'components/Modals/Reservation';
+export type InfoProps = {
+  [key: string]: string | number | undefined;
+  imgUrl: string;
+  feePerOneNight: number;
+};
 
 function HotelList() {
   const { reservationFocusModal, clickReservationModal } = useModal();
 
-  const SEARCH_FILTER = [
-    '300개 이상의 숙소',
-    '5월 12일 - 5월 18일',
-    '₩100,000~₩1,000,000',
-    '게스트 3명',
-  ];
-
-  const searchFilter = SEARCH_FILTER.map((result, index) => {
-    const key = keyMaker();
-    return (
-      <Fragment key={key}>
-        <FilterText>{result}</FilterText>
-        {index !== SEARCH_FILTER.length - 1 && <FilterText>ㆍ</FilterText>}
-      </Fragment>
-    );
-  });
+  const location = useLocation();
+  const queryString = `/accommodations${location.search}`;
+  const { fetchedData, loading } = useGetFetch(queryString);
 
   const onClickEvent = (e: React.MouseEvent<HTMLDivElement>) => {
     clickReservationModal(reservationFocusModal);
   };
 
+  const hotels = fetchedData?.map((info: InfoProps) => {
+    return (
+      <Fragment key={info.id}>
+        <Hotel onClickEvent={onClickEvent} info={info} />
+        <StyledDevider />
+      </Fragment>
+    );
+  });
+
   return (
     <>
       <HotelListWrap>
-        {searchFilter}
+        <FilterText data={fetchedData} />
         <TypographyH2>지도에서 선택한 지역의 숙소</TypographyH2>
-        <Hotel onClickEvent={onClickEvent} />
-        <StyledDevider />
-        <SkeletonHotel />
+        <List>{loading ? <SkeletonHotel /> : hotels}</List>
       </HotelListWrap>
       {reservationFocusModal && <Reservation />}
     </>
@@ -51,18 +52,20 @@ function HotelList() {
 
 const HotelListWrap = styled.div`
   width: 50%;
-  height: 100%;
+  height: 90vh;
   max-width: 732px;
   padding: 24px 32px 0 32px;
+`;
+
+const List = styled.div`
+  height: calc(90vh - 108px);
+  padding-right: 20px;
+  overflow: auto;
 `;
 
 const TypographyH2 = styled.h2`
   ${({ theme }) => theme.fontStyles.bold24px};
   margin-bottom: 32px;
-`;
-
-const FilterText = styled.span`
-  ${({ theme }) => theme.fontStyles.normal12px};
 `;
 
 const StyledDevider = styled(Divider)`
--- fe/src/components/Map/index.tsx
+++ fe/src/components/Map/index.tsx
@@ -1,46 +1,121 @@
+/* eslint-disable prefer-const */
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import styled from 'styled-components';
 import { Checkbox, ButtonGroup, Button } from '@mui/material';
 
 import { ReactComponent as PlusIcon } from 'images/FE_숙소예약서비스/plus.svg';
 import { ReactComponent as MinusIcon } from 'images/FE_숙소예약서비스/minus.svg';
-import { useEffect, useRef } from 'react';
+
+import { useGetFetch } from 'hooks/useFetch';
+import { toLocalString } from 'utils/helper';
 
 declare global {
   interface Window {
     kakao: any;
   }
 }
+
+type DataTypes = {
+  coordinateX: number;
+  coordinateY: number;
+  name: string;
+  feePerOneNight: number;
+};
+
 const { kakao }: Window = window;
 
+let kakaoMap: any;
+
+function loadMap(data: any, ref: React.RefObject<HTMLDivElement>) {
+  if (!data) return;
+  const firstData = data[0];
+
+  const mapOptions = {
+    center: new kakao.maps.LatLng(firstData.coordinateX, firstData.coordinateY),
+    level: 8,
+  };
+
+  const mapContainer = ref.current;
+  kakaoMap = new kakao.maps.Map(mapContainer, mapOptions);
+
+  data.forEach((loc: DataTypes) => {
+    const marker = new kakao.maps.CustomOverlay({
+      map: kakaoMap,
+      title: loc.name,
+      position: new kakao.maps.LatLng(loc.coordinateX, loc.coordinateY),
+      content: `
+        <div style="${customMarker}">
+          ₩${toLocalString(loc.feePerOneNight)}
+        </div>
+      `,
+    });
+  });
+}
+
 function Map() {
+  const location = useLocation();
+  const queryString = `/accommodations${location.search}`;
+  const { fetchedData } = useGetFetch(queryString);
+
   const mapRef = useRef<HTMLDivElement>(null);
 
+  const data: DataTypes[] | undefined = fetchedData;
+
+  const [isDraggabled, setDraggabled] = useState<boolean>(true);
+
+  const setDraggable = (draggable: boolean) => {
+    kakaoMap.setDraggable(!draggable);
+    setDraggabled(!isDraggabled);
+  };
+
+  const zoomIn = (e: MouseEvent<HTMLButtonElement> | undefined) => {
+    let level = kakaoMap.getLevel();
+    kakaoMap.setLevel(level - 1);
+  };
+
+  const zoomOut = (e: MouseEvent<HTMLButtonElement> | undefined) => {
+    let level = kakaoMap.getLevel();
+    kakaoMap.setLevel(level + 1);
+  };
   useEffect(() => {
-    const mapOptions = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
-    };
-    const mapContainer = mapRef.current;
-    const map = new kakao.maps.Map(mapContainer, mapOptions);
-  }, []);
+    loadMap(data, mapRef);
+  }, [fetchedData]);
 
   return (
     <MapContainer id="mapContainer" ref={mapRef}>
       <MoveMapCheck>
-        <StyledCheckbox defaultChecked sx={{ '& .MuiSvgIcon-root': { fontSize: 32 } }} />
+        <StyledCheckbox
+          defaultChecked
+          sx={{ '& .MuiSvgIcon-root': { fontSize: 32 } }}
+          onClick={() => {
+            setDraggable(isDraggabled);
+          }}
+        />
         <span>지도를 움직이며 검색하기</span>
       </MoveMapCheck>
       <MapLevelButtons orientation="vertical" aria-label="지도 확대 축소 버튼">
-        <StyledButton aria-label="지도 확대 버튼">
+        <StyledButton aria-label="지도 확대 버튼" onClick={zoomIn}>
           <PlusIcon />
         </StyledButton>
-        <StyledButton aria-label="지도 축소 버튼">
+        <StyledButton aria-label="지도 축소 버튼" onClick={zoomOut}>
           <MinusIcon />
         </StyledButton>
       </MapLevelButtons>
     </MapContainer>
   );
 }
+
+const customMarker = `
+  padding: 4px 8px;
+  border: 1px solid black;
+  border-radius: 999px;
+  background: #FFFFFF;
+  font-weight: 700;
+  font-size: '12px';
+  line-height: '17.38px';
+`;
 
 const MapContainer = styled.div`
   width: 50%;
